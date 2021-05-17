ad1cdac525c6d962e76407493009501ce56479e3
Edward Steere <edward.steere@gmail.com>
2021-05-17 14:43:02 +0200

    Get the page to load with the next-redux-wrapper installed

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import PropTypes from 'prop-types'
+
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import React from 'react'
+import { wrapper } from '../lib/redux'
+
+const Plottr = ({ Component, pageProps }) => <Component {...pageProps} />
+
+Plottr.propTypes = {
+  Component: PropTypes.func.isRequired,
+  pageProps: PropTypes.object,
 }
 
-export default MyApp
+export default wrapper.withRedux(Plottr)
--- pages/project.js
+++ pages/project.js
@@ -1,3 +1,43 @@
-const Project = () => <div>placeholder</div>
+import React from 'react'
+import { t as i18n } from 'plottr_locales'
+import { EditSeries, ErrorBoundary, BookList, FileLocation, SubNav } from 'connected-components'
+import { Nav, NavItem, Button } from 'react-bootstrap'
 
-export default Project
+export default function Project() {
+  const openDashboard = () => {
+    // TODO
+    // ipcRenderer.send('pls-open-dashboard')
+    console.warn('Still need to implement "open dashboard"')
+  }
+
+  const SubNavigation = () => {
+    return (
+      <SubNav>
+        <Nav bsStyle="pills">
+          <NavItem>
+            <Button bsSize="small" onClick={openDashboard}>
+              {i18n('Open Dashboard')}
+            </Button>
+          </NavItem>
+          <FileLocation />
+        </Nav>
+      </SubNav>
+    )
+  }
+
+  return (
+    <ErrorBoundary>
+      <div className="series__container container-with-sub-nav">
+        <SubNavigation />
+        <div className="tab-body">
+          <EditSeries />
+          <BookList />
+        </div>
+      </div>
+    </ErrorBoundary>
+  )
+}
+
+export async function getServerSideProps(context) {
+  return props
+}
