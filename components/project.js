814ffd431e6076c73d4e6913a99a0449ccff93e2
Edward Steere <edward.steere@gmail.com>
2021-05-24 11:09:15 +0200

    Remove the Open Dashboard button from the project tab

--- components/project.js
+++ components/project.js
@@ -1,25 +1,13 @@
 import React from 'react'
-import { t as i18n } from 'plottr_locales'
-import { Nav, NavItem, Button } from 'react-bootstrap'
+import { Nav } from 'react-bootstrap'
 
 import { EditSeries, ErrorBoundary, BookList, FileLocation, SubNav } from 'connected-components'
 
 export default function Project() {
-  const openDashboard = () => {
-    // TODO
-    // ipcRenderer.send('pls-open-dashboard')
-    console.warn('Still need to implement "open dashboard"')
-  }
-
   const SubNavigation = () => {
     return (
       <SubNav>
         <Nav bsStyle="pills">
-          <NavItem>
-            <Button bsSize="small" onClick={openDashboard}>
-              {i18n('Open Dashboard')}
-            </Button>
-          </NavItem>
           <FileLocation />
         </Nav>
       </SubNav>
