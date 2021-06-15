9ceac3e80740021713a8feb7625e4d410cd9c312
Edward Steere <edward.steere@gmail.com>
2021-06-15 14:22:35 +0200

    Add login forms and login API

--- components/login-form.js
+++ components/login-form.js
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import { PropTypes } from 'prop-types'
 
 import { withEventTargetValue } from '../lib/withEventTargetValue'
+import { withEventTargetPreventingDefault } from '../lib/withEventTargetPreventingDefault'
 
 const LoginForm = ({ onSubmit }) => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
   return (
-    <form onSubmit={withEventTargetValue(onSubmit)} className="login__form">
+    <form onSubmit={withEventTargetPreventingDefault(onSubmit)} className="login__form">
       <label htmlFor="email">Email</label>
       <input id="email" type="email" value={email} onChange={withEventTargetValue(setEmail)} />
       <label htmlFor="password">Password</label>
