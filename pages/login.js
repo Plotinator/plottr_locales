47732ec63c0ffd7b758662a10b5b87ec5e95b095
Edward Steere <edward.steere@gmail.com>
2021-06-16 13:14:24 +0200

    Wire up the FirebaseUI for authorisation

--- pages/login.js
+++ pages/login.js
@@ -1,18 +1,14 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { PropTypes } from 'prop-types'
 import Head from 'next/head'
 import Image from 'next/image'
 
 import { FunSpinner } from 'connected-components'
-import CreateAccountForm from '../components/create-account-form'
-import LoginForm from '../components/login-form'
-
-import { signIn, onSessionChange, createAccount } from '../lib/firebase'
+import { onSessionChange, firebaseUI, startUI } from '../lib/firebase'
 
 export default function Login() {
-  const [isCreatingAccount, setIsCreatingAccount] = useState(true)
-  const [posting, setPosting] = useState(false)
   const [sessionChecked, setSessionChecked] = useState(false)
+  const [ui, setUI] = useState(null)
 
   useEffect(() => {
     if (sessionChecked) return
@@ -24,85 +20,30 @@ export default function Login() {
     })
   }, [])
 
-  const handleClickSwitchToLogin = () => {
-    if (!posting) setIsCreatingAccount(false)
-  }
-
-  const handleClickSwitchToCreate = () => {
-    if (!posting) setIsCreatingAccount(true)
-  }
-
-  const SwitchToLogin = () => (
-    <p>
-      Create an account (or
-      <button
-        disabled={posting}
-        onClick={handleClickSwitchToLogin}
-        className="login__switch-form-type-button"
-      >
-        log in
-      </button>
-      )
-    </p>
-  )
-
-  const SwitchToCreateAccount = () => (
-    <p>
-      Log into an account (or
-      <button
-        disabled={posting}
-        onClick={handleClickSwitchToCreate}
-        className="login__switch-form-type-button"
-      >
-        create an account
-      </button>
-      )
-    </p>
-  )
-
-  const handleSubmitLoginForm = (form) => {
-    if (posting) return
-    setPosting(true)
-    const email = form['0'].value
-    const password = form['1'].value
-    // TODO: if request fails then unset posting
-    signIn(email, password)
-  }
-
-  const handleSubmitCreateAccountForm = (form) => {
-    if (posting) return
-    setPosting(true)
-    const firstName = form['0'].value
-    const email = form['1'].value
-    const password = form['2'].value
-    // TODO: if request fails then unset posting
-    createAccount(firstName, email, password).then((response) => {
-      window.location.href = '/timeline'
-    })
-  }
+  const firebaseLoginComponentRef = useRef()
+  useEffect(() => {
+    if (firebaseLoginComponentRef.current) {
+      const _ui = firebaseUI()
+      setUI(_ui)
+      startUI(_ui, '#firebase-login')
+    }
+  }, [])
 
   const renderMain = () => {
-    if (sessionChecked) {
-      return (
-        <>
-          <div className="login__left">
-            <h1>Welcome to Plottr</h1>
-            {isCreatingAccount ? <SwitchToLogin /> : <SwitchToCreateAccount />}
-            {isCreatingAccount ? (
-              <CreateAccountForm onSubmit={handleSubmitCreateAccountForm} posting={posting} />
-            ) : (
-              <LoginForm onSubmit={handleSubmitLoginForm} posting={posting} />
-            )}
+    return (
+      <>
+        <div className="login__left" style={{ hidden: !sessionChecked }}>
+          <h1>Welcome to Plottr</h1>
+          <div id="firebase-login" ref={firebaseLoginComponentRef}></div>
+        </div>
+        <div className="login__right" style={{ hidden: !sessionChecked }}>
+          <div className="login__logo">
+            <Image src="/logo_28_500.png" alt="Plottr Logo" width="358" height="500" />
           </div>
-          <div className="login__right">
-            <div className="login__logo">
-              <Image src="/logo_28_500.png" alt="Plottr Logo" width="358" height="500" />
-            </div>
-          </div>
-        </>
-      )
-    }
-    return <FunSpinner />
+        </div>
+        {sessionChecked ? null : <FunSpinner />}
+      </>
+    )
   }
 
   return (
@@ -112,7 +53,6 @@ export default function Login() {
         <meta name="description" content="Plottr" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className="login__main">{renderMain()}</main>
       <footer className="login-footer"> </footer>
     </div>
