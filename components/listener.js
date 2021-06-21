23cd629f0bf1d8198dc17cce9132f6603d0217d4
Edward Steere <edward.steere@gmail.com>
2021-06-21 15:37:51 +0200

    Wire up setting permission on file change

--- components/listener.js
+++ components/listener.js
@@ -1,14 +1,21 @@
 import { useEffect } from 'react'
+import { connect } from 'react-redux'
 
+import { actions } from 'pltr/v2'
 import { listen } from '../lib/firebase'
 
-const Listener = ({ userId, selectedFile }) => {
+const Listener = ({ userId, selectedFile, setPermission }) => {
   useEffect(() => {
     if (!userId || !selectedFile || !selectedFile.id) return
     listen(userId, selectedFile.id)
+    setPermission(selectedFile.permission)
   }, [selectedFile, userId])
 
   return null
 }
 
-export default Listener
+const {
+  permission: { setPermission },
+} = actions
+
+export default connect(null, { setPermission })(Listener)
