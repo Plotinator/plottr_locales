648b88abf84f344cad11c0eba940e8b9a42c2c90
Edward Steere <edward.steere@gmail.com>
2021-06-22 10:34:05 +0200

    Setup locale on startup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { setupI18n } from 'plottr_locales'
+
+import { localeSettings } from '../lib/locale-settings'
 
 import '../styles/globals.scss'
+
+setupI18n(localeSettings, {})
 
 const Plottr = ({ Component, pageProps }) => <Component {...pageProps} />
 
