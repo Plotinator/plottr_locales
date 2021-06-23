4acc742329f86d68ee012238961717b6ab03e098
Edward Steere <edward.steere@gmail.com>
2021-06-23 11:59:49 +0200

    Stop listening on file change and switch to newly created file

--- components/listener.js
+++ components/listener.js
@@ -3,14 +3,19 @@ import { PropTypes } from 'prop-types'
 import { connect } from 'react-redux'
 
 import { actions, selectors } from 'pltr/v2'
-import { listen } from '../lib/firebase'
+import { listen, stopListening } from '../lib/firebase'
 
 const Listener = ({ userId, selectedFile, setPermission }) => {
   useEffect(() => {
-    if (!userId || !selectedFile || !selectedFile.id || selectedFile.none) return
-    // TODO: when the file changes, stop listening
-    listen(userId, selectedFile.id)
+    if (!userId || !selectedFile || !selectedFile.id || selectedFile.none) {
+      return () => {}
+    }
+    const unsubscribeFunctions = listen(userId, selectedFile.id)
     setPermission(selectedFile.permission)
+    return () => {
+      stopListening(unsubscribeFunctions)
+      setPermission('viewer')
+    }
   }, [selectedFile, userId])
 
   return null
