168b09377a632143f230dc1e2e73afc81ee0d462
Edward Steere <edward.steere@gmail.com>
2021-06-24 15:56:47 +0200

    Use an env var for the key to connect our firebase service account

--- pages/api/new-file.js
+++ pages/api/new-file.js
@@ -1,14 +1,13 @@
 const admin = require('firebase-admin')
 
 if (!admin.apps.length) {
-  if (process.env.FIREBASE_ENV !== 'production') {
+  if (process.env.FIREBASE_ENV === 'development') {
     const projectId = 'plottr-ci'
     process.env.FIRESTORE_EMULATOR_HOST = 'localhost:8080'
     admin.initializeApp({ projectId })
-  } else {
-    // TODO: use the service-level account to connect to Firebase
-    // const serviceAccount = require('./path/to/serviceAccountKey.json')
-    admin.initializeApp()
+  } else if (process.env.FIREBASE_ENV === 'production' || process.env.FIREBASE_ENV === 'preview') {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_KEY)
+    admin.initializeApp(serviceAccount)
   }
 }
 
