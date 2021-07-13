e2088d398aef390638ce906df863ad876bc5fcaa
Edward John Steere <edward.steere@gmail.com>
2021-07-13 13:15:22 +0200

    Add support for standard templates

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { setupI18n } from 'plottr_locales'
 
 import { localeSettings } from '../lib/locale-settings'
+import { seedTemplates } from '../lib/templates'
 
 import '../styles/globals.scss'
 
 setupI18n(localeSettings, {})
 
-const Plottr = ({ Component, pageProps }) => <Component {...pageProps} />
+const Plottr = ({ Component, pageProps }) => {
+  useEffect(() => {
+    seedTemplates()
+  }, [])
+
+  return <Component {...pageProps} />
+}
 
 Plottr.propTypes = {
   Component: PropTypes.func.isRequired,
