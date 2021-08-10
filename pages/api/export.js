2f3fe68c26c37ff623b46e9545045ee64e99f705
Edward John Steere <edward.steere@gmail.com>
2021-08-10 15:50:37 +0200

    Return a value from export API

--- pages/api/export.js
+++ pages/api/export.js
@@ -5,22 +5,24 @@ export default (req, res) => {
   const file = req.body.file
   const config = req.body.config
   const extension = config.type === 'scrivener' ? 'scrivener' : 'docx'
-  askToExport(
-    `/tmp/fileToExport.${extension}`,
-    file,
-    config.type,
-    config,
-    (error, filePath) => {
-      if (error) {
-        res.status(503)
-        return res.json({ error })
-      } else {
-        res.status(200)
-        res.setHeader('Content-Disposition', `attachment;filename=${file.file.fileName}.docx`)
-        res.setHeader('Content-Type', 'application/octet-stream')
-        return fs.createReadStream(filePath).pipe(res)
-      }
-    },
-    false
-  )
+  return new Promise((resolve, reject) => {
+    askToExport(
+      `/tmp/fileToExport.${extension}`,
+      file,
+      config.type,
+      config,
+      (error, filePath) => {
+        if (error) {
+          res.status(503)
+          reject(res.json({ error }))
+        } else {
+          res.status(200)
+          res.setHeader('Content-Disposition', `attachment;filename=${file.file.fileName}.docx`)
+          res.setHeader('Content-Type', 'application/octet-stream')
+          resolve(fs.createReadStream(filePath).pipe(res))
+        }
+      },
+      false
+    )
+  })
 }
