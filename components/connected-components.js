d3c07284da9bed007a87c17fd052b4a16bdd2876
Edward John Steere <edward.steere@gmail.com>
2021-08-24 15:35:41 +0200

    Fix the version for the initial fetch

--- components/connected-components.js
+++ components/connected-components.js
@@ -110,7 +110,7 @@ const platform = {
       const selectedFile = fileList.find((thatFile) => thatFile.id === fileId)
       if (!selectedFile) return
 
-      openFile(userId, fileId, clientId, appVersion()).then(() => {
+      openFile(userId, fileId, clientId, selectedFile.version).then(() => {
         store.dispatch(actions.project.selectFile(selectedFile))
         closeDashboard()
       })
