ee38a764f3b9770390c47accd8cc2b909f166b95
Edward John Steere <edward.steere@gmail.com>
2021-08-25 12:30:43 +0200

    Remove subnav from project tab

--- components/project.js
+++ components/project.js
@@ -1,23 +1,11 @@
 import React from 'react'
-import { Nav } from 'react-bootstrap'
 
-import { EditSeries, ErrorBoundary, BookList, FileLocation, SubNav } from 'connected-components'
+import { EditSeries, ErrorBoundary, BookList } from 'connected-components'
 
 export default function Project() {
-  const SubNavigation = () => {
-    return (
-      <SubNav>
-        <Nav bsStyle="pills">
-          <FileLocation />
-        </Nav>
-      </SubNav>
-    )
-  }
-
   return (
     <ErrorBoundary>
       <div className="series__container container-with-sub-nav">
-        <SubNavigation />
         <div className="tab-body">
           <EditSeries />
           <BookList />
