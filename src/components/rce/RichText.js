091fb603c7660e4fd98561c6e5867d068af546a9
Edward John Steere <edward.steere@gmail.com>
2021-09-06 12:43:28 +0200

    WIP: fetch current value when conflict is detected

--- src/components/rce/RichText.js
+++ src/components/rce/RichText.js
@@ -29,6 +29,7 @@ const RichTextConnector = (connector) => {
           autoFocus={props.autofocus}
           selection={props.selection}
           text={props.description}
+          fetchCurrentValue={props.fetchCurrentValue}
           darkMode={props.darkMode}
         />
       )
@@ -62,6 +63,7 @@ const RichTextConnector = (connector) => {
     editable: PropTypes.bool,
     autofocus: PropTypes.bool,
     className: PropTypes.string,
+    fetchCurrentValue: PropTypes.func,
     darkMode: PropTypes.bool.isRequired,
     isStorageURL: PropTypes.func.isRequired,
     imagePublicURL: PropTypes.func.isRequired,
