97884311356ae5d1771e079967c8d23d92563d2e
Edward Steere <edward.steere@gmail.com>
2021-09-09 16:34:47 +0200

    Fix the admin app initialisation for file creation

--- pages/api/new-file.js
+++ pages/api/new-file.js
@@ -1,10 +1,13 @@
 const admin = require('firebase-admin')
 
 if (!admin.apps.length) {
-  if (process.env.FIREBASE_ENV === 'development' || process.env.FIREBASE_ENV === 'preview') {
+  if (process.env.FIREBASE_ENV === 'development') {
     const projectId = 'plottr-ci'
     process.env.FIRESTORE_EMULATOR_HOST = 'localhost:8080'
     admin.initializeApp({ projectId })
+  } else if (process.env.FIREBASE_ENV === 'preview') {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_KEY)
+    admin.initializeApp({ credential: admin.credential.cert(serviceAccount) })
   } else if (process.env.FIREBASE_ENV === 'production') {
     const serviceAccount = JSON.parse(process.env.FIREBASE_KEY)
     admin.initializeApp({ credential: admin.credential.cert(serviceAccount) })
