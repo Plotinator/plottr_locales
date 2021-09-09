e09104e2a9d9a0380e134205993a842fa20598ef
Edward Steere <edward.steere@gmail.com>
2021-09-09 16:20:05 +0200

    Fix URL for file creation

--- pages/api/new-file.js
+++ pages/api/new-file.js
@@ -1,11 +1,11 @@
 const admin = require('firebase-admin')
 
 if (!admin.apps.length) {
-  if (process.env.FIREBASE_ENV === 'development') {
+  if (process.env.FIREBASE_ENV === 'development' || process.env.FIREBASE_ENV === 'preview') {
     const projectId = 'plottr-ci'
     process.env.FIRESTORE_EMULATOR_HOST = 'localhost:8080'
     admin.initializeApp({ projectId })
-  } else if (process.env.FIREBASE_ENV === 'production' || process.env.FIREBASE_ENV === 'preview') {
+  } else if (process.env.FIREBASE_ENV === 'production') {
     const serviceAccount = JSON.parse(process.env.FIREBASE_KEY)
     admin.initializeApp({ credential: admin.credential.cert(serviceAccount) })
   }
