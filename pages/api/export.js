2474e4e1f3f1b04873f1540f910a8d5a2f70671a
Edward John Steere <edward.steere@gmail.com>
2021-09-17 11:24:36 +0200

    Use storage for export

--- pages/api/export.js
+++ pages/api/export.js
@@ -1,5 +1,28 @@
+const admin = require('firebase-admin')
 import fs from 'fs'
 import askToExport from '../../lib/exporter/start_export'
+
+if (!admin.apps.length) {
+  if (process.env.FIREBASE_ENV === 'development') {
+    const projectId = 'plottr-ci'
+    process.env.FIRESTORE_EMULATOR_HOST = 'localhost:8080'
+    process.env.FIREBASE_STORAGE_EMULATOR_HOST = 'localhost:9199'
+    admin.initializeApp({ projectId })
+  } else if (process.env.FIREBASE_ENV === 'preview') {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_KEY)
+    admin.initializeApp({ credential: admin.credential.cert(serviceAccount) })
+  } else if (process.env.FIREBASE_ENV === 'production') {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_KEY)
+    admin.initializeApp({ credential: admin.credential.cert(serviceAccount) })
+  }
+}
+
+const baseBucket =
+  process.env.FIREBASE_ENV === 'production'
+    ? 'gs://plottr.appspot.com'
+    : 'gs://plottr-ci.appspot.com'
+
+const storage = admin.storage()
 
 export default (req, res) => {
   const file = req.body.file
@@ -16,10 +39,32 @@ export default (req, res) => {
           res.status(503)
           reject(res.json({ error }))
         } else {
-          res.status(200)
-          res.setHeader('Content-Disposition', `attachment;filename=${file.file.fileName}.docx`)
-          res.setHeader('Content-Type', 'application/octet-stream')
-          resolve(fs.createReadStream(filePath).pipe(res))
+          console.log('Saved file at: ', `/tmp/fileToExport.${extension}`)
+          const bucket = storage.bucket(`${baseBucket}/tmp`)
+          bucket.upload(
+            `/tmp/fileToExport.${extension}`,
+            {
+              destination: bucket.file(`/tmp/fileToExport.${extension}`),
+              resumable: false,
+            },
+            (err, storedFile) => {
+              if (err) {
+                console.error('Error: ', err)
+                reject(err)
+                return
+              }
+              console.log(`Stored file on firestore at: tmp/${file.file.fileName}.${extension}`)
+              console.log('file: ', storedFile)
+              storedFile.makePublic().then((result) => {
+                console.log('result of make public', result)
+                const url = storedFile.publicUrl
+                console.log('Redirecting to: ', url)
+                res.status(302)
+                res.setHeader('Location', url)
+                resolve()
+              })
+            }
+          )
         }
       },
       false
