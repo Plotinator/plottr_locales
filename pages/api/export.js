13839f6381564a108c78c088f7d4749efe6c5c79
Edward John Steere <edward.steere@gmail.com>
2021-09-20 11:28:01 +0200

    Fix the name of the bucket that we're storing into

--- pages/api/export.js
+++ pages/api/export.js
@@ -40,7 +40,7 @@ export default (req, res) => {
           reject(res.json({ error }))
         } else {
           console.log('Saved file at: ', `/tmp/fileToExport.${extension}`)
-          const bucket = storage.bucket(`${baseBucket}/tmp`)
+          const bucket = storage.bucket('tmp')
           bucket.exists().then((result) => {
             const nextBucket = result[0]
               ? Promise.resolve(bucket)
