c8f1e4fef90add0d777d21a6493a476c8646e9f3
Edward John Steere <edward.steere@gmail.com>
2021-09-20 10:27:58 +0200

    Add favicon

--- components/root.js
+++ components/root.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Router, Switch, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import Head from 'next/head'
 
 import { PlottrModal } from 'connected-components'
 import { history } from '../lib/history'
@@ -47,6 +48,17 @@ const modalStyles = {
 const Root = () => {
   return (
     <Provider store={store}>
+      <Head>
+        <title>Plottr</title>
+        <meta name="description" content="Plottr" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <React.StrictMode>
         <PlottrModal isOpen={!passwordSet()} style={modalStyles}>
           <PasswordForm />
