4ddc59a90506f933a40423dc104723f3aa73cf4b
Edward Steere <edward.steere@gmail.com>
2021-10-04 10:07:30 +0200

    Fix a fat finger after downstreaming

--- src/components/dashboard/files/FileActions.js
+++ src/components/dashboard/files/FileActions.js
@@ -27,8 +27,6 @@ const FileActionsConnector = (connector) => {
 
   const osIsUnknown = os === 'unknown'
 
-  const osIsUnknown = os === 'unknown'
-
   let showInMessage = t('Show in File Explorer')
   if (isMacOS) {
     showInMessage = t('Show in Finder')
