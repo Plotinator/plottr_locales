13190d0d89c4f4ee3d84ad8fa5612c9a0449d857
Edward Steere <edward.steere@gmail.com>
2021-10-06 16:03:39 +0200

    Remove request logging

--- pages/api/verify-token.js
+++ pages/api/verify-token.js
@@ -1,5 +1,4 @@
 export const verifyToken = (auth, req) => {
   const sessionCookie = req.cookies.session || ''
-  console.log('Checking cookie: ', sessionCookie)
   return auth.verifySessionCookie(sessionCookie, true)
 }
