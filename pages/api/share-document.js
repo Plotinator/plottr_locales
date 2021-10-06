422e53935e4ca8ee0f4a2a90563c1a80cb9bfdbf
Edward Steere <edward.steere@gmail.com>
2021-10-06 10:57:47 +0200

    Secure the end points

--- pages/api/share-document.js
+++ pages/api/share-document.js
@@ -1,3 +1,5 @@
+import { verifyToken } from './verify-token'
+
 const admin = require('firebase-admin')
 
 if (!admin.apps.length) {
@@ -21,42 +23,44 @@ const auth = admin.auth()
 export default (req, res) => {
   const { fileId, emailAddress, permission, userId } = req.body
 
-  return database
-    .doc(`authorisation/${userId}/granted/${fileId}`)
-    .get()
-    .then((documentRef) => {
-      if (documentRef.exists) {
-        const data = documentRef.data()
-        if (data.permission === 'owner') {
-          return auth
-            .getUserByEmail(emailAddress)
-            .then((user) => {
-              return database
-                .doc(`authorisation/${user.uid}/granted/${fileId}`)
-                .set({ permission })
-                .then((result) => {
-                  res.status(200)
-                  res.end()
-                  return result
-                })
-            })
-            .catch((error) => {
-              res.status(400)
-              res.send('No user found with that email address')
-              return Promise.resolve('No user found with that email address')
-            })
+  return verifyToken(auth, req).then(() => {
+    return database
+      .doc(`authorisation/${userId}/granted/${fileId}`)
+      .get()
+      .then((documentRef) => {
+        if (documentRef.exists) {
+          const data = documentRef.data()
+          if (data.permission === 'owner') {
+            return auth
+              .getUserByEmail(emailAddress)
+              .then((user) => {
+                return database
+                  .doc(`authorisation/${user.uid}/granted/${fileId}`)
+                  .set({ permission })
+                  .then((result) => {
+                    res.status(200)
+                    res.end()
+                    return result
+                  })
+              })
+              .catch((error) => {
+                res.status(400)
+                res.send('No user found with that email address')
+                return Promise.resolve('No user found with that email address')
+              })
+          }
         }
-      }
-      console.error(`User <${userId}> doesn't own document <${fileId}> and can't share it.`)
-      res.status(400)
-      res.send(`We're having trouble sharing the document with that user.`)
-      return Promise.resolve(
-        `User <${userId}> doesn't own document <${fileId}> and can't share it.`
-      )
-    })
-    .catch((error) => {
-      res.status(503)
-      res.send(`Error sharing document ${fileId} to ${emailAddress}: ${error}`)
-      console.error(`Error sharing document ${fileId} to ${emailAddress}`, error)
-    })
+        console.error(`User <${userId}> doesn't own document <${fileId}> and can't share it.`)
+        res.status(400)
+        res.send(`We're having trouble sharing the document with that user.`)
+        return Promise.resolve(
+          `User <${userId}> doesn't own document <${fileId}> and can't share it.`
+        )
+      })
+      .catch((error) => {
+        res.status(503)
+        res.send(`Error sharing document ${fileId} to ${emailAddress}: ${error}`)
+        console.error(`Error sharing document ${fileId} to ${emailAddress}`, error)
+      })
+  })
 }
