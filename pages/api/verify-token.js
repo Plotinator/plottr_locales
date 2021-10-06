441cd8da31f1bee63c617de949da8f0ffa97c59f
Edward Steere <edward.steere@gmail.com>
2021-10-06 13:45:21 +0200

    TMP: debug session cookie

--- pages/api/verify-token.js
+++ pages/api/verify-token.js
@@ -1,4 +1,5 @@
 export const verifyToken = (auth, req) => {
   const sessionCookie = req.cookies.session || ''
+  console.log('Checking cookie: ', sessionCookie)
   return auth.verifySessionCookie(sessionCookie, true)
 }
