ce198a824462969164a66e3d51ea33648a37c2c5
Edward Steere <edward.steere@gmail.com>
2021-10-06 13:52:39 +0200

    TMP2: log the request cookies for new file

--- pages/api/new-file.js
+++ pages/api/new-file.js
@@ -21,6 +21,7 @@ const database = admin.firestore()
 const auth = admin.auth()
 
 export default (req, res) => {
+  console.error('Creating file.  Cookies are:', req.cookies)
   return verifyToken(auth, req).then(() => {
     const file = req.body.file
     const fileRecord = req.body.fileRecord
