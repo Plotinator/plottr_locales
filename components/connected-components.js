6f8e42ff102d21277e7b17bea0408638451b2aed
jeana fernandez <jeana.luisa@gmail.com>
2021-10-29 15:47:48 +0800

    added spinner while fetching file

--- components/connected-components.js
+++ components/connected-components.js
@@ -129,6 +129,7 @@ const platform = {
       const selectedFile = fileList.find((thatFile) => thatFile.id === fileId)
       if (!selectedFile) return
 
+      store.dispatch(actions.project.showLoader())
       openFile(userId, fileId, clientId, selectedFile.version, selectedFile.permission).then(() => {
         store.dispatch(actions.project.selectFile(selectedFile))
         closeDashboard()
