03d160158d244b2ad681dac1f604119c3f456eb4
Cameron Sutter <cameronsutter0@gmail.com>
2021-10-29 12:26:25 -0500

    add Project tab SubNav

--- components/project.js
+++ components/project.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
-import { EditSeries, ErrorBoundary, BookList } from 'connected-components'
+import { EditSeries, ErrorBoundary, BookList, SubNav } from 'connected-components'
+import { Nav, NavItem, Button } from 'react-bootstrap'
+import Download from './download'
 
 export default function Project() {
+  const SubNavigation = () => {
+    return (
+      <SubNav>
+        <Nav bsStyle="pills">
+          <Download />
+        </Nav>
+      </SubNav>
+    )
+  }
   return (
     <ErrorBoundary>
       <div className="series__container container-with-sub-nav">
+        <SubNavigation />
         <div className="tab-body">
           <EditSeries />
           <BookList />
