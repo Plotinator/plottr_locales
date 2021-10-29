3ef78c4c9bc85bb246e0b02f070cd7783cac56b8
Edward Steere <edward.steere@gmail.com>
2021-10-29 11:33:15 +0200

    Squashed 'lib/plottr_components/' changes from 98bba3af..66c7d395
    66c7d395 Merge pull request #23 from Plotinator/upstream-changes-from-plottr_electron
    6cd056a4 Add missing dependencies for new onboarding components
    8a5c5a94 Merge commit '957deb43ad996e7e3ff64fafa4d188d2eea358c6' into staging
    cf89e73b Merge pull request #22 from Plotinator/upstream-changes-to-plottr_web
    3936f359 Fix infinite image loading
    690268cb Merge remote-tracking branch 'origin/main' into staging
    6fbf4bdf fix error report vs full error reporting
    69918bb6 make create error report button functional
    b313f4c7 tweak when backup options are shown
    bc50e8e2 change timeline settings to Structure
    6d8bbec8 removed lock border
    49f9e6d5 Remove a redundant and inconsistent return
    f144db52 added temporary endpoint to add beta users
    f16a1cd4 Merge branch 'staging' of https://github.com/Plotinator/plottr_web into staging
    9f701a82 change account dashboard modals' background color
    29a759b4 Account for missing templates in characters
    a59068d7 tweak onboarding process
    8d908526 Resize images before uploading to storage
    803cc961 Add `isCloudFile` check to other upload points too
    77bda7ad Merge branch 'staging' of https://github.com/Plotinator/plottr_web into staging
    bee21c42 match web to changes on desktop
    82579561 user first interaction flow
    38fd08cc Integrate mixpanel
    7d2bbee7 Add another check for a missing file name
    a82a86f2 Protect against missing `fileName`s
    ed8503a8 tweak new project options
    a52b4ece Merge the account modals (same as in desktop)
    b74aa06d Merge branch 'staging' of https://github.com/Plotinator/plottr_web into staging
    aa42f1e9 added spinner for files and images
    fc558ce2 Merge commit '708c2b61286fafeb603b4c4c582a3b8446e3e2c3' into staging
    f1f93a16 Fix the way that we release/steal the lock
    be3cff25 fix distorted lock
    a69cf3b2 fix rcelock for notes and its customo attributes
    5bdd6b9e Change the wording for the RCE lock control
    54c1095f Release the lock when focus blurs on the current editor
    8247409e fix rce lock in modals
    
    git-subtree-dir: lib/plottr_components
    git-subtree-split: 66c7d39574cb9a2d1b8ccc7f6fa9a92f4317c3de

--- src/components/dashboard/account/proOnboarding/Uploading.js
+++ src/components/dashboard/account/proOnboarding/Uploading.js
@@ -24,7 +24,7 @@ const UploadingConnector = (connector) => {
   } = connector
 
   const Uploading = ({ nextStep, projects, templates }) => {
-    const [settings] = useSettingsInfo()
+    const [settings] = useSettingsInfo(false)
     const [maxItems, setMaxItems] = useState(100)
     const [currentProgress, setCurrentProgress] = useState(0)
     const [currentObj, setCurrentObj] = useState(null)
@@ -87,7 +87,7 @@ const UploadingConnector = (connector) => {
               `File was migrated.  Migration history: ${data.file.appliedMigrations}.  Initial version: ${data.file.initialVersion}`
             )
           }
-          return extractImages(data, settings.user.id)
+          extractImages(data, settings.user.id)
             .then((patchedData) =>
               uploadExisting(settings.user.email, settings.user.id, patchedData)
             )
