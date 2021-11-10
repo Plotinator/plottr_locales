b9573387c9f477b3025ef9f20f2b5a7bd71f349b
Edward Steere <edward.steere@gmail.com>
2021-11-10 11:17:22 +0200

    Setup i18 when exporting to remove spurious logs

--- pages/api/export.js
+++ pages/api/export.js
@@ -1,8 +1,13 @@
 const admin = require('firebase-admin')
-import askToExport from '../../lib/exporter/start_export'
 import AdmZip from 'adm-zip'
 import { v4 as uuidv4 } from 'uuid'
 import { verifyToken } from './verify-token'
+import { setupI18n } from 'plottr_locales'
+import { localeSettings } from '../../lib/locale-settings'
+
+setupI18n(localeSettings, {})
+
+import askToExport from '../../lib/exporter/start_export'
 
 if (!admin.apps.length) {
   if (process.env.FIREBASE_ENV === 'development') {
