d966cc45f05cc3078f0ce8239e9812654bb56877
Edward Steere <edward.steere@gmail.com>
2021-11-17 10:13:17 +0200

    Respond with 401 when session expires or we get an invalid cookie

--- pages/api/verify-token.js
+++ pages/api/verify-token.js
@@ -1,4 +1,9 @@
-export const verifyToken = (auth, req) => {
+export const verifyToken = (auth, req, res) => {
   const sessionCookie = req.cookies.session || ''
-  return auth.verifySessionCookie(sessionCookie, true)
+  return auth.verifySessionCookie(sessionCookie, true).catch((error) => {
+    console.error('Error authenticating request.', error.message, error)
+    res.status(401)
+    res.setHeader('Set-Cookie', `session=; Max-Age=0; HttpOnly`)
+    res.end()
+  })
 }
