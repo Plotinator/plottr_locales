a1004f8708ceb3bc5afa177f3584575e3f410ffe
Edward John Steere <edward.steere@gmail.com>
2022-01-17 17:14:26 +0200

    Implement features via `plottr_world`

--- v2/constants/ActionTypes.js
+++ v2/constants/ActionTypes.js
@@ -254,10 +254,17 @@ export const SET_USER_ID = 'SET_USER_ID'
 export const SET_CLIENT_ID = 'SET_CLIENT_ID'
 export const SET_EMAIL_ADDRESS = 'SET_EMAIL_ADDRESS'
 export const SET_HAS_PRO = 'SET_HAS_PRO'
+export const SET_IS_ON_WEB = 'SET_IS_ON_WEB'
 // IMPORTANT: Please keep this up to date(!).  Impacts our ability to
 // track actual changes to the content of a file as apposed to system
 // data.
-export const CLIENT_ACTIONS = [SET_USER_ID, SET_CLIENT_ID, SET_EMAIL_ADDRESS, SET_HAS_PRO]
+export const CLIENT_ACTIONS = [
+  SET_USER_ID,
+  SET_CLIENT_ID,
+  SET_EMAIL_ADDRESS,
+  SET_HAS_PRO,
+  SET_IS_ON_WEB,
+]
 
 // action actions (for undo)
 export const RECORD_LAST_ACTION = 'RECORD_LAST_ACTION'
