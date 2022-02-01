04fbcb29d397d08dd6fa57095d1a1335578b84ea
Cameron Sutter <cameronsutter0@gmail.com>
2022-02-01 08:07:27 -0600

    fix tag-to-pro-lifetime endpoint

--- pages/api/admin/tag-to-pro-lifetime.js
+++ pages/api/admin/tag-to-pro-lifetime.js
@@ -20,9 +20,11 @@ export default async (req, res) => {
 
   if (key != 'A2eFc15') return res.status(500).send('')
 
-  const email = req.body['contact[email]']
-  const tags = req.body['contact[tags]']
-  console.log('PRO-LIFETIME-HOOK', email, new Date().toString())
+  const { email, tag } = req.body
+
+  if (!email) {
+    return res.status(400).send({ where: 'email', error: 'no email' })
+  }
 
   // check that the contact has the right tag
   if (tag != 'Plottr: Customer - Pro - Lifetime') {
