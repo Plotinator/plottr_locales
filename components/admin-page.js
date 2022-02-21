08cc9d7164222397ebeb961c165fb5ea03bec259
Edward John Steere <edward.steere@gmail.com>
2022-02-21 12:49:03 +0200

    Scaffold out the main components of the admin page

--- components/admin-page.js
+++ components/admin-page.js
@@ -1,10 +1,15 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 import { onSessionChange, currentUser, logOut } from 'wired-up-firebase'
 
 import { logger } from '../lib/logger'
+import AdminPageLayout from './admin-page-layout'
+import AdminPageMain from './admin-page-main'
+import AdminPageNav from './admin-page-nav'
 
 const AdminPage = () => {
+  const [section, setSection] = useState('')
+
   useEffect(() => {
     let fileListener = null
     const sessionListener = onSessionChange(
@@ -36,7 +41,14 @@ const AdminPage = () => {
     }
   }, [])
 
-  return <div>Hi!</div>
+  return (
+    <main>
+      <AdminPageLayout>
+        <AdminPageNav section={section} setSection={setSection} />
+        <AdminPageMain section={section} />
+      </AdminPageLayout>
+    </main>
+  )
 }
 
 export default AdminPage
