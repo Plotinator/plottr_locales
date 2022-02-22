eaa6f5a7b0c13463833ebc5cfb4a3ca3475b00c6
Edward Steere <edward.steere@gmail.com>
2022-02-22 08:57:12 +0200

    Remove the close cross from the dashboard

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 import { PropTypes } from 'prop-types'
 import { connect } from 'react-redux'
 import cx from 'classnames'
-import { VscChromeClose } from 'react-icons/vsc'
 
 import { selectors } from 'pltr/v2'
 import { DashboardBody, DashboardNav } from 'connected-components'
@@ -34,11 +33,7 @@ const Dashboard = ({ darkMode, closeDashboard, ignoreSignalsToClose }) => {
     <div id="dashboard__react__root">
       <div className={cx('dashboard__main', { darkmode: darkMode })}>
         <DashboardNav currentView={activeView} setView={setActiveView} />
-        <DashboardBody currentView={activeView} setView={setActiveView} darkMode={darkMode}>
-          <div className="dashboard__close-button">
-            <VscChromeClose onClick={closeDashboard} />
-          </div>
-        </DashboardBody>
+        <DashboardBody currentView={activeView} setView={setActiveView} darkMode={darkMode} />
       </div>
     </div>
   )
