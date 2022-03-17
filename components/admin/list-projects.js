bbc50141717fd1fb5085ee0348436d921650fb92
Cameron Sutter <cameronsutter0@gmail.com>
2022-03-17 09:15:39 -0500

    share and unshare with support user

--- components/admin/list-projects.js
+++ components/admin/list-projects.js
@@ -1,8 +1,11 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { FormGroup, ControlLabel, FormControl, Label, Button } from 'react-bootstrap'
 import axios from 'axios'
 // import UserRecord from './UserRecord'
 import ProjectList from './ProjectList'
+import { AdminErrorBoundary } from './admin-error-boundary'
+
+const supportEmail = 'support@plottr.com'
 
 const withEventTargetValue = (f) => (event) => {
   return f(event.target.value)
@@ -12,9 +15,39 @@ const ListProjects = () => {
   const [files, setFiles] = useState([])
   // const [userRecord, setUserRecord] = useState(null)
   const [error, setError] = useState([])
-  const [userEmail, setUserEmail] = useState(null)
+  const [userEmail, setUserEmail] = useState('')
+  const [supportUserData, setSupportUserData] = useState(null)
+
+  useEffect(() => {
+    fetchSupportProjects()
+  }, [])
+
+  const fetchAll = () => {
+    if (!supportUserData) {
+      fetchSupportProjects()
+    }
+
+    fetchProjects()
+  }
+
+  const fetchSupportProjects = () => {
+    axios
+      .post('/api/admin/projects-read', { email: supportEmail })
+      .then((response) => {
+        if (response?.data?.files) {
+          console.log(response.data)
+          setSupportUserData(response.data)
+        }
+      })
+      .catch((error) => {
+        console.log('Support User', error)
+        console.log('Support User', error.response.data)
+      })
+  }
 
   const fetchProjects = () => {
+    if (!userEmail) return null
+
     axios
       .post('/api/admin/projects-read', { email: userEmail })
       .then((response) => {
@@ -60,10 +93,21 @@ const ListProjects = () => {
           />
           <FormControl.Feedback />
         </FormGroup>
-        <Button onClick={fetchProjects}>Fetch!</Button>
+        <Button onClick={fetchAll}>Fetch!</Button>
       </div>
       <p />
-      {!error ? <ProjectList files={files} /> : <Label>{error.message}</Label>}
+      {!error ? (
+        <AdminErrorBoundary>
+          <ProjectList
+            files={files}
+            supportUserProjects={supportUserData?.projects}
+            supportID={supportUserData?.user.uid}
+            refetch={fetchAll}
+          />
+        </AdminErrorBoundary>
+      ) : (
+        <Label>{error.message}</Label>
+      )}
       {renderUserRecord()}
       <style jsx>{`
         .list-files {
