1d51c32a19960d643b6373449385c45f2e141422
Cameron Sutter <cameronsutter0@gmail.com>
2022-03-17 09:15:39 -0500

    share and unshare with support user

--- components/admin/ProjectList.js
+++ components/admin/ProjectList.js
@@ -1,8 +1,55 @@
 import { Table, Label, Button } from 'react-bootstrap'
 import { PropTypes } from 'prop-types'
+import axios from 'axios'
+import { SUPPORT_EMAIL } from '../../lib/admin/constants'
 
-const ProjectList = ({ files }) => {
+const ProjectList = ({ files, supportUserProjects, refetch, supportID }) => {
+  const shareWithSupport = (fileId, currentShareRecords) => {
+    axios
+      .post('/api/admin/project-share-support', { fileId, currentShareRecords, supportID })
+      .then((response) => {
+        console.log(response)
+        refetch()
+      })
+      .catch((error) => {
+        console.log(error)
+        console.log(error.response.data)
+        refetch()
+      })
+  }
+
+  const unShareWithSupport = (fileId, currentShareRecords) => {
+    axios
+      .post('/api/admin/project-share-support', {
+        fileId,
+        currentShareRecords,
+        supportID,
+        unshare: true,
+      })
+      .then((response) => {
+        console.log(response)
+        refetch()
+      })
+      .catch((error) => {
+        console.log(error)
+        console.log(error.response.data)
+        refetch()
+      })
+  }
+
+  const isSharedWithSupport = (fileId, shareRecords) => {
+    const supportIsAuthorized = fileId && supportUserProjects.some((id) => id == fileId)
+    const supportOnShareRecord =
+      shareRecords && shareRecords.some((rec) => rec.emailAddress == SUPPORT_EMAIL)
+
+    // NOTE: I could see making sense as either && or ||
+    // this way captures more cases
+    return supportIsAuthorized || supportOnShareRecord
+  }
+
   const renderTime = (frbTimeStamp) => {
+    if (!frbTimeStamp) return null
+
     const realDate = new Date(frbTimeStamp._seconds * 1000)
     return realDate.toLocaleString()
   }
@@ -21,20 +68,37 @@ const ProjectList = ({ files }) => {
         </tr>
       </thead>
       <tbody>
-        {files.map(({ fileId, deleted, fileName, permission, version, timeStamp }) => (
-          <tr key={fileId}>
-            <td>{deleted ? <Label bsStyle="danger">DELETED</Label> : ''}</td>
-            <td>{fileName}</td>
-            <td>{renderTime(timeStamp)}</td>
-            <td>{fileId}</td>
-            <td>{permission}</td>
-            <td>{version}</td>
-            <td>
-              <Button style={{ marginRight: '8px' }}>Share to Support@</Button>
-              {deleted ? <Button bsStyle="warning">Undelete</Button> : null}
-            </td>
-          </tr>
-        ))}
+        {files.map(
+          ({ fileId, deleted, fileName, permission, version, timeStamp, shareRecords }) => (
+            <tr key={fileId}>
+              <td>{deleted ? <Label bsStyle="danger">DELETED</Label> : ''}</td>
+              <td>{fileName}</td>
+              <td>{renderTime(timeStamp)}</td>
+              <td>{fileId}</td>
+              <td>{permission}</td>
+              <td>{version}</td>
+              <td>
+                {isSharedWithSupport(fileId, shareRecords) ? (
+                  <Button
+                    style={{ marginRight: '8px' }}
+                    bsStyle="warning"
+                    onClick={() => unShareWithSupport(fileId, shareRecords)}
+                  >
+                    Un-Share with Support User
+                  </Button>
+                ) : (
+                  <Button
+                    style={{ marginRight: '8px' }}
+                    onClick={() => shareWithSupport(fileId, shareRecords)}
+                  >
+                    Share to Support User
+                  </Button>
+                )}
+                {deleted ? <Button bsStyle="warning">Undelete</Button> : null}
+              </td>
+            </tr>
+          )
+        )}
       </tbody>
     </Table>
   )
@@ -42,6 +106,9 @@ const ProjectList = ({ files }) => {
 
 ProjectList.propTypes = {
   files: PropTypes.array,
+  supportUserProjects: PropTypes.array,
+  refetch: PropTypes.func,
+  supportID: PropTypes.string,
 }
 
 export default ProjectList
