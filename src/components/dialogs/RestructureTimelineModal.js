ba7792b20163d031a0f2c054cab95aed3ae7b1b9
Edward John Steere <edward.steere@gmail.com>
2023-03-02 09:07:55 +0200

    Get everything in place for the new modal

---
+++ src/components/dialogs/RestructureTimelineModal.js
@@ -0,0 +1,117 @@
+import React, { useState } from 'react'
+import { PropTypes } from 'prop-types'
+
+import { t } from 'plottr_locales'
+
+import UnconnectedPlottrModal from '../PlottrModal'
+import { checkDependencies } from '../checkDependencies'
+
+const modalStyles = {
+  overlay: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  content: {
+    width: '75%',
+    position: 'relative',
+    left: 'auto',
+    bottom: 'auto',
+    right: 'auto',
+    minHeight: 500,
+    maxHeight: 'calc(100vh - 120px)',
+    borderRadius: 20,
+  },
+}
+
+const RestructureTimelineModalConnector = (connector) => {
+  const UnconnectedBeatRow = (connector) => {
+    const BeatRow = ({ id, title, hierarchyLevel }) => {
+      return (
+        <tr>
+          <td>{title}</td>
+          <td>{hierarchyLevel.name}</td>
+        </tr>
+      )
+    }
+
+    BeatRow.propTypes = {
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      hierarchyLevel: PropTypes.object.isRequired,
+    }
+
+    const { redux } = connector
+    checkDependencies({ redux })
+
+    if (redux) {
+      const {
+        pltr: { selectors },
+      } = connector
+      const { connect } = redux
+      const uniqueBeatTitleSelector = selectors.makeBeatTitleSelector()
+
+      return connect((state, ownProps) => ({
+        title: uniqueBeatTitleSelector(state.present, ownProps.id),
+        hierarchyLevel: selectors.hierarchyLevelSelector(state.present, ownProps.beatId),
+      }))(BeatRow)
+    }
+
+    throw new Error('Could not connect BeatRow')
+  }
+
+  const PlottrModal = UnconnectedPlottrModal(connector)
+  const BeatRow = UnconnectedBeatRow(connector)
+
+  const RestructureTimelineModal = ({ closeDialog, beats }) => {
+    const [stagedBeats, setStagedBeats] = useState(beats)
+
+    return (
+      <PlottrModal isOpen={true} onRequestClose={closeDialog} style={modalStyles}>
+        <table>
+          <thead>
+            <tr>
+              <th>{t('Name')}</th>
+              <th>{t('Structure Type')}</th>
+            </tr>
+          </thead>
+          {stagedBeats.map(({ id }) => {
+            return (
+              <React.Fragment key={id}>
+                <BeatRow id={id} />
+              </React.Fragment>
+            )
+          })}
+        </table>
+      </PlottrModal>
+    )
+  }
+
+  RestructureTimelineModal.propTypes = {
+    closeDialog: PropTypes.func.isRequired,
+    beats: PropTypes.array.isRequired,
+  }
+
+  const { redux } = connector
+  checkDependencies({ redux })
+
+  if (redux) {
+    const {
+      pltr: { selectors, actions },
+    } = connector
+    const { connect } = redux
+    return connect(
+      (state) => ({
+        sortedHierarchyLevels: selectors.sortedHierarchyLevels(state.present),
+        beats: selectors.visibleSortedBeatsForTimelineByBookSelector(state.present),
+      }),
+      {
+        closeDialog: actions.ui.closeRestructureModal,
+      }
+    )(RestructureTimelineModal)
+  }
+
+  throw new Error('Could not connect RestructureTimelineModal')
+}
+
+export default RestructureTimelineModalConnector
