7b8e7d7f77cfc1d852fc6460133d0b099403e599
Edward Steere <edward.steere@gmail.com>
2023-03-06 12:37:58 +0200

    Refactor exporting outline into directives and interpreters

--- src/exporter/word/exporters/itemTemplates.js
+++ src/exporter/word/exporters/itemTemplates.js
@@ -3,6 +3,28 @@ import { Paragraph } from 'docx'
 import { serialize } from './to_word'
 
 export default function exportItemTemplates(item, headingLevel) {
+  return interpret(exportItemTemplatesDirectives(item, headingLevel))
+}
+
+function interpret(directives) {
+  return directives.flatMap(({ type, ...props }) => {
+    switch (type) {
+      case 'paragraph': {
+        return [new Paragraph({ text: props.text })]
+      }
+
+      case 'function': {
+        return props.func()
+      }
+
+      default: {
+        return []
+      }
+    }
+  })
+}
+
+export function exportItemTemplatesDirectives(item, headingLevel) {
   return (item.templates || []).flatMap((t) => {
     return (t.attributes || []).flatMap((attr) => {
       const value =
@@ -11,12 +33,11 @@ export default function exportItemTemplates(item, headingLevel) {
         })?.value || attr.value
       let paragraphs = []
       if (!value) return []
-      paragraphs.push(new Paragraph({ text: attr.name, heading: headingLevel }))
+      paragraphs.push({ type: 'paragraph', text: attr.name, heading: headingLevel })
       if (Array.isArray(value)) {
-        const attrParagraphs = serialize(value)
-        paragraphs = [...paragraphs, ...attrParagraphs]
+        paragraphs = [...paragraphs, { type: 'function', func: () => serialize(value) }]
       } else {
-        paragraphs.push(new Paragraph({ text: value }))
+        paragraphs.push({ type: 'paragraph', text: value })
       }
       return paragraphs
     })
