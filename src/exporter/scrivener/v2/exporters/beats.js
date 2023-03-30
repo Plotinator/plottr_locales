e2fa06a0d09dbc23d8573b56fc182d71e6f6b6e5
Edward John Steere <edward.steere@gmail.com>
2023-03-30 12:03:19 +0200

    Export to scrivener with Acts and Chapters as parent folders

--- src/exporter/scrivener/v2/exporters/beats.js
+++ src/exporter/scrivener/v2/exporters/beats.js
@@ -1,4 +1,4 @@
-import { keyBy, includes } from 'lodash'
+import { keyBy, includes, sortBy } from 'lodash'
 import { t } from 'plottr_locales'
 import {
   buildDescriptionFromObject,
@@ -6,7 +6,7 @@ import {
   createTextBinderItem,
   buildTemplateProperties,
 } from '../utils'
-import { helpers, selectors } from 'pltr/v2'
+import { helpers, selectors, tree } from 'pltr/v2'
 
 const {
   sortedLinesByBookSelector,
@@ -14,6 +14,7 @@ const {
   sortedBeatsByBookSelector,
   makeBeatTitleSelector,
   cardsCustomAttributesSelector,
+  beatsByBookSelector,
 } = selectors
 const {
   card: { sortCardsInBeat, cardMapping },
@@ -22,6 +23,7 @@ const {
 export default function exportBeats(state, documentContents, options) {
   // get current book id and select only those beats/lines/cards
   const beats = sortedBeatsByBookSelector(state)
+  const beatTree = beatsByBookSelector(state)
   const lines = sortedLinesByBookSelector(state)
   const card2Dmap = cardMapSelector(state)
   const beatCardMapping = cardMapping(beats, lines, card2Dmap, null)
@@ -55,82 +57,101 @@ export default function exportBeats(state, documentContents, options) {
 
   // create a BinderItem for each beat (Type: Folder)
   //   create a BinderItem for each card (Type: Text)
+  //
+  // Nest each beat inside a parent if it has one.
+  const topLevelBeats = []
+  const sortByBeatPosition = (xs) => {
+    return sortBy(xs, 'position')
+  }
+  const _allBeats = tree.sortingReduce('id', sortByBeatPosition)(
+    beatTree,
+    (acc, beat, parentId) => {
+      const uniqueBeatTitleSelector = makeBeatTitleSelector(state)
+      const title = uniqueBeatTitleSelector(state, beat.id)
+      const { binderItem } = createFolderBinderItem(title)
+      acc[beat.id] = binderItem
+      if (acc[parentId]) {
+        acc[parentId].Children.BinderItem.push(binderItem)
+      } else if (parentId !== null) {
+        throw new Error('Invalid tree detected while exporting')
+      } else if (parentId === null) {
+        topLevelBeats.push(binderItem)
+      }
 
-  return filteredBeats.flatMap((beat) => {
-    const uniqueBeatTitleSelector = makeBeatTitleSelector(state)
-    const title = uniqueBeatTitleSelector(state, beat.id)
-    const { binderItem } = createFolderBinderItem(title)
+      if (options.outline.sceneCards) {
+        // sort cards into beats by lines (like outline auto-sorting)
+        const cards = beatCardMapping[beat.id]
+        const sortedCards = sortCardsInBeat(beat.autoOutlineSort, cards, lines)
+        const filteredCards = getFilteredCards(sortedCards)
 
-    if (options.outline.sceneCards) {
-      // sort cards into beats by lines (like outline auto-sorting)
-      const cards = beatCardMapping[beat.id]
-      const sortedCards = sortCardsInBeat(beat.autoOutlineSort, cards, lines)
-      const filteredCards = getFilteredCards(sortedCards)
+        filteredCards.forEach((c) => {
+          const { id, binderItem: cardItem } = createTextBinderItem(c.title)
+          binderItem['Children']['BinderItem'].push(cardItem)
 
-      filteredCards.forEach((c) => {
-        const { id, binderItem: cardItem } = createTextBinderItem(c.title)
-        binderItem['Children']['BinderItem'].push(cardItem)
+          // save card info into documentContents
+          let title = ''
+          const lineId = c.lineId
+          const line = linesById[lineId]
+          if (line) title = line.title
 
-        // save card info into documentContents
-        let title = ''
-        const lineId = c.lineId
-        const line = linesById[lineId]
-        if (line) title = line.title
+          let descObj = {}
 
-        let descObj = {}
-
-        if (options.outline.customAttributes) {
-          customAttrs.reduce((acc, entry) => {
-            acc[entry.name] = c[entry.name]
-            return acc
-          }, descObj)
-        }
-
-        if (options.outline.templates) {
-          c.templates.forEach((t) => {
-            t.attributes.forEach((attr) => {
-              if (descObj[attr.name]) {
-                descObj[`${t.name}:${attr.name}`] = attr.value
-              } else {
-                descObj[attr.name] = attr.value
-              }
-            })
-          })
-        }
-
-        if (options.outline.where == 'notes') {
-          descObj.description = c.description
-        }
-
-        let description = buildDescriptionFromObject(descObj, options.outline)
-
-        // handle template properties
-        if (options.outline.templates) {
-          description = [
-            ...description,
-            ...buildDescriptionFromObject(buildTemplateProperties(c.templates), true),
-          ]
-        }
-
-        const contents = {
-          docTitle: options.outline.plotlineInTitle ? t('Plotline: {title}', { title }) : null,
-          description: description,
-        }
-        documentContents[id] = {
-          notes: contents,
-        }
-
-        if (options.outline.where != 'notes') {
-          documentContents[id][options.outline.where] = {
-            description: buildDescriptionFromObject(
-              { description: c.description },
-              options.outline
-            ),
+          if (options.outline.customAttributes) {
+            customAttrs.reduce((acc, entry) => {
+              acc[entry.name] = c[entry.name]
+              return acc
+            }, descObj)
           }
-        }
-      })
-    }
 
-    return binderItem
-  })
+          if (options.outline.templates) {
+            c.templates.forEach((t) => {
+              t.attributes.forEach((attr) => {
+                if (descObj[attr.name]) {
+                  descObj[`${t.name}:${attr.name}`] = attr.value
+                } else {
+                  descObj[attr.name] = attr.value
+                }
+              })
+            })
+          }
+
+          if (options.outline.where == 'notes') {
+            descObj.description = c.description
+          }
+
+          let description = buildDescriptionFromObject(descObj, options.outline)
+
+          // handle template properties
+          if (options.outline.templates) {
+            description = [
+              ...description,
+              ...buildDescriptionFromObject(buildTemplateProperties(c.templates), true),
+            ]
+          }
+
+          const contents = {
+            docTitle: options.outline.plotlineInTitle ? t('Plotline: {title}', { title }) : null,
+            description: description,
+          }
+          documentContents[id] = {
+            notes: contents,
+          }
+
+          if (options.outline.where != 'notes') {
+            documentContents[id][options.outline.where] = {
+              description: buildDescriptionFromObject(
+                { description: c.description },
+                options.outline
+              ),
+            }
+          }
+        })
+      }
+
+      return acc
+    },
+    {}
+  )
+
+  return topLevelBeats
 }
