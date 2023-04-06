6ce7a9c029bbd2e79c38b46962c6adfa62d65a72
Edward John Steere <edward.steere@gmail.com>
2023-04-06 07:47:31 +0200

    Fix edge cases and dynamic uses of pltr

--- v2/selectors/index.js
+++ v2/selectors/index.js
@@ -1,14 +1,19 @@
 import allSelectors from './allSelectors'
 
 const selectors = (selectSubState) => {
-  return Object.entries(allSelectors)
-    .reduce((selectorsAcc, nextEntry) => {
-      const [name, selector] = nextEntry
-      return {
-        ...selectorsAcc,
-        [name]: (state, ...args) => selector(selectSubState(state), ...args),
-      }
-    }, {})
+  return Object.entries(allSelectors).reduce((selectorsAcc, nextEntry) => {
+    const [name, selector] = nextEntry
+    return {
+      ...selectorsAcc,
+      [name]:
+        typeof selector.memoizedResultFunc === 'undefined' && selector.length === 0
+          ? () => {
+              const actualSelector = selector()
+              return (state, ...args) => actualSelector(selectSubState(state), ...args)
+            }
+          : (state, ...args) => selector(selectSubState(state), ...args),
+    }
+  }, {})
 }
 
 export default selectors
