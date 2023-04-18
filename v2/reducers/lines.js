77621b092b533eebb1dc6916a5e90a463355b872
Jeana Fernandez <jeana.luisa@gmail.com>
2023-04-18 18:11:29 +0800

    fix wrong line color

--- v2/reducers/lines.js
+++ v2/reducers/lines.js
@@ -101,18 +101,12 @@ const lines =
       }
 
       case DUPLICATE_BOOK: {
-        const linesInBook = state.filter((l) => l.bookId == action.id)
-        const nextPosition = nextPositionInBook(linesInBook, actionBookId)
         const newLines = action.newLines
           .filter(({ bookId }) => bookId !== 'series') // this is to protect against a bad template that unnecessarily had a series line
           .map((l, index) => {
             const newLine = cloneDeep(l)
             newLine.id = action.nextLineId + newLine.id // give it a new id
-            newLine.bookId = actionBookId // add it to the new/current book
-            newLine.position = nextPosition + newLine.position // put it in the right position
-            if (!newLine.color || newLine.color == nextColor(0)) {
-              newLine.color = nextColor(linesInBook.length + index)
-            }
+            newLine.bookId = action.newBookId // add it to the new/current book
             return newLine
           })
         return [...state, ...newLines]
