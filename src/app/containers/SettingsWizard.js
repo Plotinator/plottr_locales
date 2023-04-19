cf2db654ae26e7c32e2548a43266b619dbb87cee
Cameron Sutter <cameronsutter0@gmail.com>
2023-04-19 16:08:01 -0500

    fix linting warnings

--- src/app/containers/SettingsWizard.js
+++ src/app/containers/SettingsWizard.js
@@ -1,8 +1,6 @@
 import React from 'react'
-import { PropTypes } from 'prop-types'
 import { connect } from 'react-redux'
 
-import { selectors, actions } from 'pltr/v2'
 import { SettingsWizard as OnboardingSettingsWizard } from 'connected-components'
 
 const SettingsWizard = () => {
