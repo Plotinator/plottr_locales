8cd871b75f752cdc919aa5289f1739dd9bd9297f
Jeana Fernandez <jeana.luisa@gmail.com>
2023-04-20 11:18:11 +0800

    fix duplicate names

--- v2/helpers/books.js
+++ v2/helpers/books.js
@@ -1,1 +1,12 @@
 export const isSeries = (id) => id === 'series'
+
+export const getCopyName = (arr, name, entity, counter = 0) => {
+  let copyName = counter ? `${name} ${counter}` : name
+  const hasDuplicate = arr.some((item) => {
+    return item[entity] && item[entity]?.indexOf(copyName) !== -1
+  })
+  if (hasDuplicate) {
+    return getCopyName(arr, name, entity, counter + 1)
+  }
+  return copyName
+}
