21455f36c2e22c25257153e05568f1c2479d85ea
Cameron Sutter <cameronsutter0@gmail.com>
2023-04-26 11:10:37 -0500

    add logic to selectors for backups table

--- v2/selectors/backupsFirstOrder.js
+++ v2/selectors/backupsFirstOrder.js
@@ -3,10 +3,12 @@
 // dependencies.
 
 import { createSelector } from 'reselect'
-import { sortBy } from 'lodash'
+import { sortBy, groupBy } from 'lodash'
 
 import { parseStringDate } from '../helpers/date'
 import { fullFileStateSelector } from './fullFileFirstOrder'
+
+import { t } from 'plottr_locales'
 
 export const backupFoldersSelector = createSelector(
   fullFileStateSelector,
@@ -27,7 +29,37 @@ const sortFolders = (folders) => {
     return parseStringDate(folder.date)
   }).reverse()
 }
-export const sortedBackupFoldersSelector = createSelector(
+const groupableName = (fileObj) => {
+  if (fileObj.storagePath) {
+    return fileObj.fileId
+  } else {
+    return fileObj.name.replace('(start-session)-', '').replace('.pltr', '')
+  }
+}
+const makeDateString = (dateObj, makeShort) => {
+  let dateStr = ''
+  try {
+    const date = dateObj instanceof Date ? dateObj : parseStringDate(dateObj)
+    const style = makeShort ? '{date, date, monthDay}' : '{date, date, medium}'
+    dateStr = t(style, { date })
+  } catch (error) {
+    console.error(error)
+  }
+  return dateStr
+}
+export const groupedSortedBackupFoldersSelector = createSelector(
   nonEmptyBackupFoldersSelector,
-  (backupFolders) => sortFolders(backupFolders)
+  (backupFolders) => {
+    const sortedFolders = sortFolders(backupFolders)
+    return sortedFolders.map((f) => {
+      const groups = groupBy(f.backups, groupableName)
+      // const groupsWithName = Object.entries(f.groups).map((group) => {})
+      return {
+        ...f,
+        groups,
+        longDateStr: makeDateString(f.date, false),
+        shortDateStr: makeDateString(f.date, true),
+      }
+    })
+  }
 )
