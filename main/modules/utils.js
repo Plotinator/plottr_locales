0bd25be24917632eb4617958b717fd89534766de
Jeana Fernandez <jeana.luisa@gmail.com>
2023-04-29 00:05:29 +0800

    use system as default theme; detech if current theme should use dark mode

--- main/modules/utils.js
+++ main/modules/utils.js
@@ -1,6 +1,6 @@
 import path from 'path'
 import { v4 as uuidv4 } from 'uuid'
-import { dialog, app, screen, BrowserWindow } from 'electron'
+import { dialog, app, screen, BrowserWindow, nativeTheme } from 'electron'
 import windowStateKeeper from 'electron-window-state'
 import i18n from 'plottr_locales'
 import log from 'electron-log'
@@ -44,6 +44,11 @@ function makeBrowserWindow(fileURL) {
 
   return currentSettings()
     .then((settings) => {
+      const backgroundColor =
+        (settings.user?.dark === 'system' && nativeTheme.shouldUseDarkColors) ||
+        settings.user?.dark === 'dark'
+          ? '#666666'
+          : '#f7f7f7'
       let config = {
         x: stateKeeper.x,
         y: stateKeeper.y,
@@ -62,7 +67,7 @@ function makeBrowserWindow(fileURL) {
         },
       }
 
-      config.backgroundColor = settings.user?.dark === 'dark' ? '#666666' : '#f7f7f7'
+      config.backgroundColor = backgroundColor
 
       // Create the browser window
       let newWindow = new BrowserWindow(config)
