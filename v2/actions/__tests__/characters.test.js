8a134d42f650740ba993a2392549484049c909d5
Jeana Fernandez <jeana.luisa@gmail.com>
2023-05-06 12:03:53 +0800

    move unit test to actions

--- v2/actions/__tests__/characters.test.js
+++ v2/actions/__tests__/characters.test.js
@@ -11,11 +11,16 @@ const {
   allCharacterAttributesSelector,
   characterAttributesSelector,
   singleCharacterSelector,
+  allBookIdsSelector,
+  allCharactersSelector,
+  allBooksWithCharactersSortedByBookAllIdsPositionSelector,
 } = selectors(pltrAdaptor)
 
 const wiredUpActions = actions(pltrAdaptor)
 const { loadFile } = wiredUpActions.ui
-const { editCharacterAttributeValue } = wiredUpActions.character
+const { editCharacterAttributeValue, addCharacter } = wiredUpActions.character
+const { addBook } = wiredUpActions.book
+const addBookToCharacter = wiredUpActions.character.addBook
 
 const EMPTY_FILE = emptyFile('Test file')
 
@@ -31,6 +36,37 @@ const initialStore = () => {
     )
   )
   return store
+}
+
+const exampleBook1 = {
+  title: 'Book',
+  premise: 'First book',
+  genre: 'Suspense',
+  theme: 'Sequel',
+}
+const exampleBook5 = {
+  title: 'Last Book',
+  premise: 'Last Sequel',
+  genre: 'Suspense',
+  theme: 'Sequel',
+}
+const exampleBook2 = {
+  title: 'Second Book',
+  premise: 'Sequel 1',
+  genre: 'Suspense',
+  theme: 'Sequel',
+}
+const exampleBook3 = {
+  title: 'Third Book',
+  premise: 'Sequel 2',
+  genre: 'Suspense',
+  theme: 'Sequel',
+}
+const exampleBook4 = {
+  title: 'Fourth Book',
+  premise: 'Sequel 3',
+  genre: 'Suspense',
+  theme: 'Sequel',
 }
 
 const ignoringChangesWeDontCareAbout = (state) => {
@@ -111,6 +147,76 @@ describe('editCharacterAttributeValue', () => {
             name: 'Species',
           },
         ])
+      })
+    })
+  })
+})
+
+describe('characterDeleteBook', () => {
+  describe('given the state with emptyFile', () => {
+    describe('and add 2 characters', () => {
+      const store = initialStore()
+      store.dispatch(addCharacter('character 1'))
+      store.dispatch(addCharacter('character 2'))
+      describe('and add 5 books', () => {
+        store.dispatch(
+          addBook(exampleBook1.title, exampleBook1.premise, exampleBook1.genre, exampleBook1.theme)
+        )
+        store.dispatch(
+          addBook(exampleBook2.title, exampleBook2.premise, exampleBook2.genre, exampleBook2.theme)
+        )
+        store.dispatch(
+          addBook(exampleBook3.title, exampleBook3.premise, exampleBook3.genre, exampleBook3.theme)
+        )
+        store.dispatch(
+          addBook(exampleBook4.title, exampleBook4.premise, exampleBook4.genre, exampleBook4.theme)
+        )
+        store.dispatch(
+          addBook(exampleBook5.title, exampleBook5.premise, exampleBook5.genre, exampleBook5.theme)
+        )
+        const stateAfterCharactersAndBooksAdd = store.getState()
+        it('should return the goldilocks characters', () => {
+          const allCharacters = allCharactersSelector(stateAfterCharactersAndBooksAdd)
+          expect(allCharacters).toHaveLength(2)
+        })
+        it('should have all the 5 new books added', () => {
+          const allBookIds = allBookIdsSelector(stateAfterCharactersAndBooksAdd)
+          expect(allBookIds).toHaveLength(6)
+        })
+        describe('and attach books randomly to characters', () => {
+          store.dispatch(addBookToCharacter(1, 2))
+          store.dispatch(addBookToCharacter(1, 6))
+          store.dispatch(addBookToCharacter(1, 3))
+          store.dispatch(addBookToCharacter(1, 4))
+          store.dispatch(addBookToCharacter(1, 5))
+          store.dispatch(addBookToCharacter(2, 2))
+          store.dispatch(addBookToCharacter(2, 6))
+          store.dispatch(addBookToCharacter(2, 3))
+          store.dispatch(addBookToCharacter(2, 4))
+          store.dispatch(addBookToCharacter(2, 5))
+          const stateAfterAddingBooks = store.getState()
+          const character1 = singleCharacterSelector(stateAfterAddingBooks, 1)
+          const character2 = singleCharacterSelector(stateAfterAddingBooks, 2)
+          const allBookIds = allBookIdsSelector(stateAfterAddingBooks)
+          const allBooksWithCharactersOrdered =
+            allBooksWithCharactersSortedByBookAllIdsPositionSelector(stateAfterAddingBooks)
+
+          it('should have attached the 5 books attach to each character', () => {
+            expect(character1.bookIds).toHaveLength(5)
+            expect(character2.bookIds).toHaveLength(5)
+          })
+
+          it('character bookIds should have same order as the book bookIds', () => {
+            const stringifiedAllBookIds = allBookIds.map((id) => String(id))
+            const allBooksWithCharactersIdsOrdered = allBooksWithCharactersOrdered.map(({ id }) =>
+              String(id)
+            )
+            const filteredBookIds = stringifiedAllBookIds.filter((id) =>
+              allBooksWithCharactersIdsOrdered.includes(id)
+            )
+            expect(filteredBookIds).toEqual(allBooksWithCharactersIdsOrdered)
+          })
+        })
       })
     })
   })
