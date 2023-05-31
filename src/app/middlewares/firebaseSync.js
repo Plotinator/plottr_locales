29876030da43c40e3d65b2427f05b909c18493e3
Edward John Steere <edward.steere@gmail.com>
2023-05-31 07:44:31 +0200

    Sync cards back up per card

--- src/app/middlewares/firebaseSync.js
+++ src/app/middlewares/firebaseSync.js
@@ -8,7 +8,7 @@ const firebaseSync = (store) => (next) => (action) => {
   const isCloudFile = selectors.isCloudFileSelector(store.getState())
   if (isCloudFile) {
     return middlewares.externalSync(identity)(overwrite, (key, data) => {
-      return ARRAY_KEYS.indexOf(key) !== -1 ? toFirestoreArray(data) : data
+      return key !== 'cards' && ARRAY_KEYS.indexOf(key) !== -1 ? toFirestoreArray(data) : data
     })(store)(next)(action)
   }
   return next(action)
