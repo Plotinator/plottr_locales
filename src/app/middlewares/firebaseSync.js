b28fbd9b486a635ccf798bbd9674317ccdfdf79d
Edward John Steere <edward.steere@gmail.com>
2023-06-01 15:34:17 +0200

    Don't turn flat array objects into arrays anymore

--- src/app/middlewares/firebaseSync.js
+++ src/app/middlewares/firebaseSync.js
@@ -4,11 +4,15 @@ import { middlewares, ARRAY_KEYS } from 'pltr/v2'
 import { overwrite, toFirestoreArray } from 'wired-up-firebase'
 import { selectors } from 'wired-up-pltr'
 
+const FLAT_ARRAY_KEYS = ['cards', 'notes', 'places', 'characters']
+
 const firebaseSync = (store) => (next) => (action) => {
   const isCloudFile = selectors.isCloudFileSelector(store.getState())
   if (isCloudFile) {
     return middlewares.externalSync(identity)(overwrite, (key, data) => {
-      return key !== 'cards' && ARRAY_KEYS.indexOf(key) !== -1 ? toFirestoreArray(data) : data
+      return FLAT_ARRAY_KEYS.indexOf(key) === -1 && ARRAY_KEYS.indexOf(key) !== -1
+        ? toFirestoreArray(data)
+        : data
     })(store)(next)(action)
   }
   return next(action)
