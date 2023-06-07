f8beb3ed03e87bded0d40ca283ef135a0f1feeb6
Edward John Steere <edward.steere@gmail.com>
2023-06-07 14:14:00 +0200

    Simplify and fix quit/save logic

--- src/components/dialogs/AskToSaveModal.js
+++ src/components/dialogs/AskToSaveModal.js
@@ -9,17 +9,14 @@ import getTestIds from '../getTestIds'
 
 export const testIds = getTestIds()
 
-export default function AskToSaveModal({ save, dontSave, cancel }) {
+export default function AskToSaveModal({ save }) {
   return (
-    <Modal show={true} onHide={cancel} dialogClassName="center-modal-vertically">
-      <Modal.Header closeButton>{i18n('Save before closing?')}</Modal.Header>
+    <Modal show={true} onHide={save} dialogClassName="center-modal-vertically">
+      <Modal.Header closeButton>{i18n('There are unsaved changes')}</Modal.Header>
       <Modal.Body>
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
           <Button data-testid={testIds.ok} bsStyle="success" onClick={save}>
             {i18n('Save')}
-          </Button>
-          <Button data-testid={testIds.cancel} bsStyle="danger" onClick={dontSave}>
-            {i18n('Exit Without Saving')}
           </Button>
         </div>
       </Modal.Body>
@@ -29,6 +26,4 @@ export default function AskToSaveModal({ save, dontSave, cancel }) {
 
 AskToSaveModal.propTypes = {
   save: PropTypes.func,
-  dontSave: PropTypes.func,
-  cancel: PropTypes.func,
 }
