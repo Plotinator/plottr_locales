acb8f7c2b7498da27f48cde555e827c85105ad9e
Jeana Fernandez <jeana.luisa@gmail.com>
2023-06-20 00:35:46 +0800

    adjusted backup tabs styles

--- src/components/dashboard/backups/BackupsProjectRow.js
+++ src/components/dashboard/backups/BackupsProjectRow.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react'
+import React from 'react'
 import PropTypes from 'react-proptypes'
 
-import Col from '../../Col'
 import Row from '../../Row'
 import UnconnectedBackupFileDisplay from './BackupFileDisplay'
 
@@ -9,36 +8,27 @@ const BackupsProjectRowConnector = (connector) => {
   const BackupFileDisplay = UnconnectedBackupFileDisplay(connector)
 
   const BackupsProjectRow = ({ folder, groupName, files }) => {
-    const [showActions, setShowActions] = useState(false)
-
     const renderFiles = () => {
       // NOTE: this works because the 'start session' version always comes first
       return files.map((file, index) => {
         return (
-          <Col key={index} xs={12} sm={6} md={3} className="dashboard__backups__project-backup">
-            <BackupFileDisplay
-              folder={folder}
-              groupName={groupName}
-              file={file}
-              folderDate={folder.shortDateStr}
-              showActions={showActions}
-            />
-          </Col>
+          <BackupFileDisplay
+            folder={folder}
+            groupName={groupName}
+            file={file}
+            folderDate={folder.shortDateStr}
+            key={`${folder.shortDateStr}-${groupName}-${file.fileId}-${index}`}
+          />
         )
       })
     }
 
     return (
-      <Row
-        key={groupName}
-        className="dashboard__backups__project-row"
-        onMouseEnter={() => setShowActions(true)}
-        onMouseLeave={() => setShowActions(false)}
-      >
-        <Col xs={12} md={6}>
+      <Row key={groupName} className="dashboard__backups__project-row">
+        <div>
           <h6>{groupName}</h6>
-        </Col>
-        {renderFiles()}
+        </div>
+        <div>{renderFiles()}</div>
       </Row>
     )
   }
