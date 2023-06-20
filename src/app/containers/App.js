3bfa6b04d42eefa5ae4868f962481bfb3f626746
Edward John Steere <edward.steere@gmail.com>
2023-06-20 11:47:39 +0200

    Add the root error boundary back

--- src/app/containers/App.js
+++ src/app/containers/App.js
@@ -215,16 +215,16 @@ const App = ({
   }
 
   return (
-    <>
+    <ErrorBoundary>
       <React.StrictMode>
         <Navigation forceProjectDashboard={forceProjectDashboard} />
       </React.StrictMode>
       <main
         className="project-main tour-end"
-        /* onClick={(event) => { */
-        /*   // The other part of this click handler is in Navigation */
-        /*   clickOnDom(event.clientX, event.clientY) */
-        /* }} */
+        onClick={(event) => {
+          // The other part of this click handler is in Navigation
+          clickOnDom(event.clientX, event.clientY)
+        }}
       >
         <React.StrictMode>
           <Body />
@@ -239,7 +239,7 @@ const App = ({
         {renderAdvanceExportModal()}
         {renderImagePickerModal()}
       </React.StrictMode>
-    </>
+    </ErrorBoundary>
   )
 }
 
