bd679b93fc75179db65288120b51d5a7211e1cd9
Edward Steere <edward.steere@gmail.com>
2023-06-28 14:05:02 +0200

    Port infini branch to mobile

--- native/index.js
+++ native/index.js
@@ -75,7 +75,11 @@ const database = () => {
       }
     },
     onSnapshot: (ref, handleSnapshot) => {
-      return ref.onSnapshot(handleSnapshot)
+      if (ref.type === 'query') {
+        return translate(ref).onSnapshot(handleSnapshot)
+      } else {
+        return ref.onSnapshot(handleSnapshot)
+      }
     },
     getDoc: (ref) => {
       return ref.get()
