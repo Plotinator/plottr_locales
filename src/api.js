6546e72ac2538f0ab43ad17bdfd1dcbd04f3a3ff
Edward John Steere <edward.steere@gmail.com>
2023-07-04 11:38:18 +0200

    Default the last opened & timestamp on a file when there is none

--- src/api.js
+++ src/api.js
@@ -643,8 +643,8 @@ const api = (
           authorisedDocuments.push({
             id: authorisation.id,
             ...data,
-            timeStamp: helpers.time.convertFromNanosAndSeconds(data.timeStamp).getTime(),
-            lastOpened: helpers.time.convertFromNanosAndSeconds(data.lastOpened).getTime(),
+            timeStamp: helpers.time.convertFromNanosAndSecondsOrDefault(data.timeStamp).getTime(),
+            lastOpened: helpers.time.convertFromNanosAndSecondsOrDefault(data.lastOpened).getTime(),
             fileURL: `plottr://${authorisation.id}`,
             isCloudFile: true,
           })
@@ -670,8 +670,8 @@ const api = (
         authorisedDocuments.push({
           id: authorisation.id,
           ...data,
-          timeStamp: helpers.time.convertFromNanosAndSeconds(data.timeStamp).getTime(),
-          lastOpened: helpers.time.convertFromNanosAndSeconds(data.lastOpened).getTime(),
+          timeStamp: helpers.time.convertFromNanosAndSecondsOrDefault(data.timeStamp).getTime(),
+          lastOpened: helpers.time.convertFromNanosAndSecondsOrDefault(data.lastOpened).getTime(),
           fileURL: `plottr://${authorisation.id}`,
           isCloudFile: true,
         })
