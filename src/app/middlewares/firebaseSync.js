060568c2ac0eeaef386f4750ecdd578f6d00e50f
Edward Steere <edward.steere@gmail.com>
2023-09-01 10:11:47 +0200

    Prevent the user from quitting when Firebase requests are in-flight

--- src/app/middlewares/firebaseSync.js
+++ src/app/middlewares/firebaseSync.js
@@ -6,20 +6,47 @@ const FLAT_ARRAY_KEYS = ['cards', 'notes', 'places', 'characters']
 
 const externalSync = middlewares.externalSync(selectPresentState)
 
-const firebaseSync = (store) => (next) => (action) => {
-  if (!action.type || SYSTEM_REDUCER_ACTION_TYPES.indexOf(action.type) !== -1) {
-    return next(action)
-  } else {
-    const isCloudFile = selectors.isCloudFileSelector(store.getState())
-    if (isCloudFile) {
-      return externalSync(overwrite, deleteSingle, (key, data) => {
-        return FLAT_ARRAY_KEYS.indexOf(key) === -1 && ARRAY_KEYS.indexOf(key) !== -1
-          ? toFirestoreArray(data)
-          : data
-      })(store)(next)(action)
-    } else {
-      return next(action)
-    }
+const firebaseSync = () => {
+  const inflightRequests = {
+    counter: 0,
+  }
+
+  const overwritePreventingDefault = (...args) => {
+    inflightRequests.counter++
+    return overwrite(...args).finally(() => {
+      inflightRequests.counter = Math.max(0, inflightRequests.counter - 1)
+    })
+  }
+
+  const deleteSinglePreventingDefault = (...args) => {
+    inflightRequests.counter++
+    return deleteSingle(...args).finally(() => {
+      inflightRequests.counter = Math.max(0, inflightRequests.counter - 1)
+    })
+  }
+
+  return {
+    inflightRequests,
+    firebaseMiddleware: (store) => (next) => (action) => {
+      if (!action.type || SYSTEM_REDUCER_ACTION_TYPES.indexOf(action.type) !== -1) {
+        return next(action)
+      } else {
+        const isCloudFile = selectors.isCloudFileSelector(store.getState())
+        if (isCloudFile) {
+          return externalSync(
+            overwritePreventingDefault,
+            deleteSinglePreventingDefault,
+            (key, data) => {
+              return FLAT_ARRAY_KEYS.indexOf(key) === -1 && ARRAY_KEYS.indexOf(key) !== -1
+                ? toFirestoreArray(data)
+                : data
+            }
+          )(store)(next)(action)
+        } else {
+          return next(action)
+        }
+      }
+    },
   }
 }
 
