3d260414c38631e8690b963fcea319ae08a00f34
Edward John Steere <edward.steere@gmail.com>
2024-05-30 09:33:59 +0200

    Fix upload sourcemaps script

--- build/upload-sourcemaps.sh
+++ build/upload-sourcemaps.sh
@@ -8,24 +8,18 @@ post_server_item=$ROLLBAR_POST_SERVER_ITEM
 
 echo "Uploading source maps for version $version!"
 
-for path in $(find bin -name "*.map"); do
-  js_file=${path%.*}
-  url=https://raw.githubusercontent.com/Plotinator/pltr_sourcemaps/${js_file}.js
-  source_map=$path
+pushd bin
+for path in $(find . -name "*.map"); do
+  js_file=$(basename ${path%.*})
+  url=https://raw.githubusercontent.com/Plotinator/pltr_sourcemaps/main/${version}/${js_file}.js
+  source_map="@$path"
 
   echo "Uploading source map for $url"
-
-  echo curl --silent --show-error https://api.rollbar.com/api/1/sourcemap \
-    -F access_token=$post_server_item \
-    -F version=$version \
-    -F minified_url=$url \
-    -F source_map=$source_map \
-    > /dev/null
 
   curl --silent --show-error https://api.rollbar.com/api/1/sourcemap \
     -F access_token=$post_server_item \
     -F version=$version \
     -F minified_url=$url \
-    -F source_map=$source_map \
-    > /dev/null
+    -F source_map=$source_map
 done
+popd
